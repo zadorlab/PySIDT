8d1d59e74a3cf94d9eb28a0db417df4853fb82d0
Matt Johnson <mjohnson541@gmail.com>
2026-04-30 15:47:29 -0700

    add function for computing local atomic configuration space sizes based on allowed atoms

--- pysidt/utils.py
+++ pysidt/utils.py
@@ -1,3 +1,4 @@
+import math
 try:
     from molecule.molecule import Molecule
 except:
@@ -36,3 +37,29 @@ def find_shortest_paths(start, end, path=None):
             paths = newpaths
     
     return None
+
+def get_local_atom_configuration_number(valence_map):
+    valence_to_available_elements = {q:sum(v for k,v in valence_map.items() if k >= q) for q in valence_map.keys()}
+    valence_to_config_number = dict()
+    
+    for i in valence_map.keys():
+        if i == 1:
+            valence_to_config_number[i] = sum(v for k,v in valence_map.items())
+        elif i == 2:
+            valence_to_config_number[i] = math.comb(valence_to_available_elements[1]+2-1,2) + valence_to_available_elements[2]
+        elif i == 3:
+            valence_to_config_number[i] = 0
+            valence_to_config_number[i] += math.comb(valence_to_available_elements[1]+3-1,3)
+            valence_to_config_number[i] += valence_to_available_elements[2]*valence_to_available_elements[1] 
+            valence_to_config_number[i] += valence_to_available_elements[3]
+        elif i == 4:
+            valence_to_config_number[i] = 0
+            valence_to_config_number[i] += math.comb(valence_to_available_elements[1]+4-1,4)
+            valence_to_config_number[i] += math.comb(valence_to_available_elements[1]+2-1,2) * valence_to_available_elements[2]
+            valence_to_config_number[i] += math.comb(valence_to_available_elements[2]+2-1,2)
+            valence_to_config_number[i] += valence_to_available_elements[3]*valence_to_available_elements[1]
+            valence_to_config_number[i] += valence_to_available_elements[4]
+        else:
+            raise ValueError
+
+    return valence_to_config_number
