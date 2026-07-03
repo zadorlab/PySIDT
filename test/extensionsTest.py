a1c8a17da1b4c54dbe39e3e0fa74886212905edd
Matt Johnson <mjohnson541@gmail.com>
2026-07-03 15:05:05 -0700

    add test for molecular_specify_internal_new_bond_extensions

--- test/extensionsTest.py
+++ test/extensionsTest.py
@@ -34,4 +34,20 @@ class TestExtensionGeneration:
                     break
             else:
                 assert False, f"Extension {m.to_smiles()} not generated in molecular_transform_atom_extensions from propane"
-            
+    
+    def test_molecular_specify_internal_new_bond_extensions(self):
+        
+        mol = Molecule().from_smiles("CCC")
+        exts = molecular_specify_internal_new_bond_extensions(mol, 0, 1, 1, "", [1.0,2.0,3.0], 
+                        max_ring_gen_size=5, tree=None, estimate_delta=False, assoc_decomposition_init_value_unc_dict=None)
+        
+        ms = [ext[0] for ext in exts]
+        msout = [Molecule(smiles=sm) for sm in ["CCC","C1CC1C","C1CCC1C","C1CCCC1C"]]
+        
+        for m in ms:
+            for mout in msout:
+                if m.is_isomorphic(mout,save_order=True):
+                    break
+            else:
+                assert False, f"Extension {m.to_smiles()} not generated in molecular_specify_internal_new_bond_extensions from propane"
+    
